f48309f2e772eda35f392bb6ca0a8ba3ee38ffd8
ElviinThomasEldho <elviin.t.eldho@gmail.com>
2025-04-17 22:34:29 +0530

    minor fix

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -24,7 +24,7 @@ const SERVICES = [
 
 const highlights = [
   "Build your <strong>success story</strong> to get the <strong>recruiters' attention</strong>.",
-  "<strong>Optimises your Keywords</strong> and making your Resume <strong>ATS-Compliant</strong>.",
+  "<strong>Optimise your Keywords</strong> and making your Resume <strong>ATS-Compliant</strong>.",
   "Presents your <strong>experience summary</strong> with <strong>key skills & competencies</strong>.",
   "Effectively <strong>structures & highlights</strong> your <strong>Responsibilities & achievements</strong>.",
 ];
